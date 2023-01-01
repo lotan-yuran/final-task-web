9b05a1a88e752b125241cbc6f2e806a000e71ae8
lotan yuran <lotan759@gmail.com>
2023-01-02 01:07:39 +0200

    add route of find all products by categoryId

--- Back/routes/productRoutes.js
+++ Back/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the product model
+// Models
 const Product = require('../models/product');
 const Category = require('../models/category');
 
-// Define the product routes
+// Routes
 router.get('/', async (req, res) => {
   const products = await Product.find().populate('category');
   res.send(products);
@@ -15,6 +15,12 @@ router.post('/', (req, res) => {
   const { name, price, description, imageURL, categoryId: category } = req.body;
   Product.create({ name, price, description, imageURL, category });
   res.send('Created');
+});
+
+router.get('/category/:categoryId', async (req, res) => {
+  const { categoryId } = req.params;
+  const products = await Product.find({ category: categoryId }).populate('category');
+  res.send(products);
 });
 
 module.exports = router;
