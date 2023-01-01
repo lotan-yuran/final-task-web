7294e44b258d5357fd1b4cdbff446a6f3b9bfeaa
lotan yuran <lotan759@gmail.com>
2023-01-02 00:20:23 +0200

    move product routes to external file

--- Back/index.js
+++ Back/index.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Models
-const Product = require("./models/product");
-const Order = require("./models/order");
+// // Models
+// const Product = require("./models/product");
+// const Order = require("./models/order");
+
+// Import routes
+const productRoutes = require('./routes/productRoutes');
+// const orderRoutes = require('./orderRoutes');
 
 const app = express();
 require("dotenv").config();
@@ -27,21 +31,23 @@ mongoose
     console.log(err);
   });
 
-app.get("/product", async (req, res) => {
-  const products = await Product.find();
-  res.send(products);
-});
+  app.use(productRoutes);
+
+// app.get("/product", async (req, res) => {
+//   const products = await Product.find();
+//   res.send(products);
+// });
 
 app.get("/order", async (req, res) => {
   const orders = await Order.find().populate("products");
   res.send(orders);
 });
 
-app.post("/product", (req, res) => {
-  const { name, price, description, imageURL } = req.body;
-  Product.create({ name, price, description, imageURL });
-  res.send("Created");
-});
+// app.post("/product", (req, res) => {
+//   const { name, price, description, imageURL } = req.body;
+//   Product.create({ name, price, description, imageURL });
+//   res.send("Created");
+// });
 
 app.post("/order", (req, res) => {
   Order.create({ products: req.body.products, name: req.body.name, phone: req.body.phone });
---
+++ Back/routes/productRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Import the product model
+const Product = require('./models/product');
+
+// Define the product routes
+router.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+router.post('/', (req, res) => {
+  const { name, price, description, imageURL } = req.body;
+  Product.create({ name, price, description, imageURL });
+  res.send('Created');
+});
+
+module.exports = router;
