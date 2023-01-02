945036748b138131d306f4b0ca0616f24786bd77
lotan yuran <lotan759@gmail.com>
2023-01-02 16:12:26 +0200

    add comments to each route

--- Back/routes/categoryRoutes.js
+++ Back/routes/categoryRoutes.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const Category = require('../models/category');
 
 // Routes
+
+// Get all categories
 router.get("/", async (req, res) => {
     const categories = await Category.find();
     res.send(categories);
   });
   
+// Add category
 router.post("/", async (req, res) => {
     const { name } = req.body;
     await Category.create({ name });
--- Back/routes/orderRoutes.js
+++ Back/routes/orderRoutes.js
@@ -5,17 +5,21 @@ const router = express.Router();
 const Order = require('../models/order');
 
 // Routes
+
+// Get all the not deleted products
 router.get("/", async (req, res) => {
     const orders = await Order.find({ deletedAt: { $ne: null } }).populate("products");
     res.send(orders);
   });
   
+// Add product
 router.post("/", async (req, res) => {
     const { products, name, address, phone } = req.body;
     await Order.create({ products, name, address, phone, orderedAt: new Date(), deletedAt: null });
     res.send("Created");
   });
 
+// Delete order by orderId (set deletedAt to current date)
 router.delete("/:orderId", async (req, res) => {
     const { orderId } = req.params;
     await Order.updateOne({ _id: orderId }, { deletedAt: Date.now() });
