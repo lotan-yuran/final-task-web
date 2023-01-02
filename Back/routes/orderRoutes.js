d5d31ab4705de2bf43a8d699f06ae60c3a7e95c9
lotan yuran <lotan759@gmail.com>
2023-01-02 15:47:23 +0200

    fix the get orders route to return only the not deleted route

--- Back/routes/orderRoutes.js
+++ Back/routes/orderRoutes.js
@@ -6,11 +6,17 @@ const Order = require('../models/order');
 
 // Routes
 router.get("/", async (req, res) => {
-    const orders = await Order.find().populate("products");
+    const orders = await Order.find({ deletedAt: { $ne: null } }).populate("products");
     res.send(orders);
   });
   
 router.post("/", async (req, res) => {
+    const { products, name, address, phone } = req.body;
+    await Order.create({ products, name, address, phone, orderedAt: new Date(), deletedAt: null });
+    res.send("Created");
+  });
+
+router.delete("/", async (req, res) => {
     const { products, name, address, phone } = req.body;
     await Order.create({ products, name, address, phone, orderedAt: new Date(), deletedAt: null });
     res.send("Created");
