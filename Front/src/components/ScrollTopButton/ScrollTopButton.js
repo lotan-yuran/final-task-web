f686f1dd3ace4e943dd58833729a132f27c447a8
LihiJosef <lihiyosef6@gmail.com>
2023-01-05 09:33:40 +0200

    jfjfjf

--- Front/src/components/ScrollTopButton/ScrollTopButton.js
+++ Front/src/components/ScrollTopButton/ScrollTopButton.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { ArrowUpward } from "@mui/icons-material";
 import { StyledButtom } from "./ScrollTopButton.style";
 import useScrollPosition from "../../hooks/scrollPosition";
-
+ 
 export const ScrollTopButton = () => {
   const scrollPosition = useScrollPosition();
   const [visible, setVisible] = useState(false);
