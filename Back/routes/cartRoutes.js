8ded721863514f0dfe58e8b43d6d61a6852233cd
LihiJosef <lihiyosef6@gmail.com>
2023-01-19 00:21:34 +0200

    crate scraping scripts

--- Back/routes/cartRoutes.js
+++ Back/routes/cartRoutes.js
@@ -1,50 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Models
-const Cart = require('../models/cart');
+const Cart = require("../models/cart");
 
 // Routes
 
 // Get cart by user id
 router.get("/:userId", async (req, res) => {
-    try {
-        const { userId } = req.params;
-        let cart = await Cart.findOne({ userId });
-        
-        if (!cart) {
-            res.send([]);
-            return;
-        }
+  try {
+    const { userId } = req.params;
+    let cart = await Cart.findOne({ userId });
 
-        cart = await cart.populate("products.product");
-        const activeProducts = cart.products.filter(
-            item => item.product.isActive === true
-            );
-        res.send(activeProducts);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!cart) {
+      res.send([]);
+      return;
     }
-  });
+
+    cart = await cart.populate("products.product");
+    const activeProducts = cart.products.filter(
+      (item) => item.product.isActive === true
+    );
+    res.send(activeProducts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Set cart by user id
 router.put("/:userId", async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const { products } = req.body;
-        const cart = await Cart.findOne({ userId });
-        
-        if (cart) {
-            await Cart.updateOne({ userId }, { products });
-        } else {
-            await Cart.create({ userId, products });
-        }
+  try {
+    const { userId } = req.params;
+    const { products } = req.body;
+    const cart = await Cart.findOne({ userId });
 
-        res.send("Updated")  
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    } 
+    if (cart) {
+      await Cart.updateOne({ userId }, { products });
+    } else {
+      await Cart.create({ userId, products });
+    }
+
+    res.send("Updated");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-  
-module.exports = router;
 
+module.exports = router;
