885b594a9c608b8a299ce3e7b110cddc5f3fc574
SivanElias <siveli123@gmail.com>
2023-01-21 17:39:16 +0200

    add websocket in admin screen

---
+++ Front/src/components/LiveData/LiveData.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+
+// const URL = "ws://127.0.0.1:3001";
+export const LiveData = () => {
+  const [connectedUsers, setConnectedUsers] = useState(0);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3001");
+
+    ws.onopen = e => {
+      ws.send("connectMe");
+    };
+
+    ws.onmessage = function (event) {
+      const users = event.data;
+      console.log("connectedUsers: ", users);
+      setConnectedUsers(users);
+    };
+
+    //clean up function
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <>
+      <Box>Number of connected Admin users: {connectedUsers} </Box>
+    </>
+  );
+};
