c8e5ae62e2e5f6e53a1ad8032c013e0b118faa5f
LihiJosef <lihiyosef6@gmail.com>
2023-01-21 22:37:14 +0200

    add single product page

--- Front/src/pages/index.js
+++ Front/src/pages/index.js
@@ -2,5 +2,6 @@ export * from "./Cart";
 export * from "./Store";
 export * from "./Login";
 export * from "./Admin";
+export * from "./Product";
 export * from "./Profile";
 export * from "./Register";
