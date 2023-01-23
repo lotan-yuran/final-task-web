055cf9bd8da532b3c4826024f22143e1f5e7f1b0
LihiJosef <lihiyosef6@gmail.com>
2023-01-23 11:47:00 +0200

    use real user userId for orders and add loader

--- Front/src/pages/Profile/Profile.js
+++ Front/src/pages/Profile/Profile.js
@@ -1,29 +1,37 @@
-import { Box, Typography } from "@mui/material";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../Recoil";
 import { useEffect, useState } from "react";
 import { OrderItems } from "../../components";
 import orderService from "../../services/orderService";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 export const Profile = () => {
+  const user = useRecoilValue(userState);
   const [orders, setOrders] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
+    setLoader(true);
     orderService
-      .getUserOrders("322592973")
+      .getUserOrders(user.email)
       .then(data => {
         setOrders(data);
       })
       .catch(err => {
         console.error(err);
         alert("Failed to get you orders, try again later");
-      });
-  }, []);
+      })
+      .finally(() => setLoader(false));
+  }, [user]);
 
   return (
     <>
       <h1>My Orders</h1>
       {orders.length > 0 && <h4>Please notice you may cancel an order within 48 hours </h4>}
       <Box sx={{ m: 3 }}>
-        {orders.length > 0 ? (
+        {loader ? (
+          <CircularProgress />
+        ) : orders.length > 0 ? (
           orders.map(order => <OrderItems key={order._id} order={order} setOrders={setOrders} />)
         ) : (
           <Typography variant="subtitle1" component="div">
