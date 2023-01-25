eb849a837d4e37b81b3ab392bc667924b5a39ccc
SivanElias <siveli123@gmail.com>
2023-01-25 13:08:59 +0200

    Add reponsive container to graph

--- Front/src/components/OrdersChart/OrdersBarChart.js
+++ Front/src/components/OrdersChart/OrdersBarChart.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { CartesianGrid, Legend, Tooltip, XAxis, YAxis, BarChart, Bar } from "recharts";
+import { CartesianGrid, Legend, Tooltip, XAxis, YAxis, BarChart, Bar, ResponsiveContainer } from "recharts";
 import orderService from "../../services/orderService";
 
 export const OrdersBarChart = () => {
@@ -14,23 +14,23 @@ export const OrdersBarChart = () => {
   }, []);
 
   return (
-    <BarChart
-      width={730}
-      height={400}
-      data={ordersCountByUser}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="userId" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="numOfOrders" fill="#8884d8" barSize={20} />
-    </BarChart>
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        data={ordersCountByUser}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="userId" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="numOfOrders" fill="#8884d8" barSize={20} />
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
