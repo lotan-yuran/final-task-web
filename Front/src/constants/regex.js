40adef4d44476e909f1f8be26570af60e38f9915
LihiJosef <lihiyosef6@gmail.com>
2023-01-28 20:24:49 +0200

    fix email regex

--- Front/src/constants/regex.js
+++ Front/src/constants/regex.js
@@ -1,3 +1,3 @@
 export const mobilePhoneRegex = /^(05)([0-9]{8})$/;
 
-export const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+[.][A-Za-z.]{2,}$/;
